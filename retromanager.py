46e2eb5aedf8a6f41b63f8b932226b0f73abfba7
Pernatsch <mpernats@dxc.com>
2019-11-18 21:50:14 +0100

    Small code polishing

--- retromanager.py
+++ retromanager.py
@@ -27,14 +27,6 @@ def setupUsage():
 	import argparse
 	parser = argparse.ArgumentParser(description="Library to generate and manipulate overlay image files and other configuration files for the RetroArch emulator frontend.")
 	
-	
-	#parser.add_argument("command", help="Main command to execute", choices=['overlay_generate', 'config_generate', 'shader_generate', 'generate_all'])
-	#parser.add_argument("core", help="The RetroArch core")
-	#parser.add_argument("gamename", help="The MAME game name")
-
-	#parser.add_argument("-tx", "--targetsizex", type=int, help="Target X size of the generated overlay")
-	#parser.add_argument("-ty", "--targetsizey", type=int, help="Target Y size of the generated overlay")
-
 	parent_parser = argparse.ArgumentParser(add_help=False)
 	parent_parser.add_argument("core", help="The RetroArch core")
 	parent_parser.add_argument("gamename", help="The MAME game name")
@@ -42,21 +34,21 @@ def setupUsage():
 	subparsers = parser.add_subparsers(dest='command', title='commands', description='valid commands', help='add -h after the command name to display help about the command')
 	
 	parser_overlay_generate = subparsers.add_parser('overlay_generate', description="Generate overlay images and configuration file", help="Generate overlay files", parents=[parent_parser])
-	#parser_overlay_generate.add_argument("core", help="The RetroArch core")
-	#parser_overlay_generate.add_argument("gamename", help="The MAME game name")
 	parser_overlay_generate.add_argument("-tx", "--targetsizex", type=int, default=0, help="target X size of the generated overlay")
 	parser_overlay_generate.add_argument("-ty", "--targetsizey", type=int, default=0, help="target Y size of the generated overlay")
 
 	parser_config_generate = subparsers.add_parser('config_generate', description="Generate RetroArch configuration files", help="Generate configuration files", parents=[parent_parser])
-	#parser_config_generate.add_argument("core", help="The RetroArch core")
-	#parser_config_generate.add_argument("gamename", help="The MAME game name")
 	
 	parser_shader_generate = subparsers.add_parser('shader_generate', description="Generate shader configuration files", help="Generate shader files", parents=[parent_parser])
-	#parser_shader_generate.add_argument("core", help="The RetroArch core")
-	#parser_shader_generate.add_argument("gamename", help="The MAME game name")
 
 	parser_shader_generate = subparsers.add_parser('generate_all', description="Performs config_generate, overlay_generate, shader_generate", help="Generate all files", parents=[parent_parser])
 
+	parser_overlay_resize = subparsers.add_parser('overlay_resize', description="Resize overlay images and generate related configuration files", help="Resize overlay files", parents=[parent_parser])
+	parser_overlay_resize.add_argument("-tx", "--targetsizex", type=int, default=0, help="target X size of the generated overlay")
+	parser_overlay_resize.add_argument("-ty", "--targetsizey", type=int, default=0, help="target Y size of the generated overlay")
+	parser_overlay_resize.add_argument("-mx", "--marginx", type=int, default=0, help="left and right margins of the generated overlay")
+	parser_overlay_resize.add_argument("-my", "--marginy", type=int, default=0, help="top and bottom margins of the generated overlay")
+	
 	args = parser.parse_args()
 	
 	if not vars(args):
@@ -81,6 +73,8 @@ def main():
 		overlaymanager.genCfg(args.core, args.gamename, config)
 		overlaymanager.genOverlay(args.core, args.gamename, config)
 		overlaymanager.genShader(args.core, args.gamename, config)
+	elif 'overlay_resize' == args.command:
+		overlaymanager.resize(args.core, args.gamename, args.targetsizex, args.targetsizey, args.marginx, args.marginy, config)
 	else:
 		print('Command not supported')
 		return -1
